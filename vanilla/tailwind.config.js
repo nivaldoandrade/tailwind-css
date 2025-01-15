fbba242d80255ad4c3840f8ce96887bb970378bd
nivaldoandrade <nivaldoandradef@gmail.com>
2025-01-15 17:13:09 -0300

    feat: customize tailwindcss theme with nasa color palette
    - Updated tailwind.config.js to define the extended color palette
    - Updated index.html to test the new custom colors with various text classes

--- vanilla/tailwind.config.js
+++ vanilla/tailwind.config.js
@@ -1,4 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"]
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        nasa: {
+          red: {
+            DEFAULT: "#8E1616",
+            primary: "#D84040",
+            secondary: "#1D1616"
+          },
+          black: {
+            DEFAULT: "#000000",
+            primary: "#8d9797",
+            secondary: "#4e545c"
+          }
+        }
+      }
+    }
+  }
 }
