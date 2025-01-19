eed406050b69b7d58bfb4f235f979bf2e6527431
nivaldoandrade <nivaldoandradef@gmail.com>
2025-01-19 15:23:52 -0300

    feat: create custom tailwind plugin
    - Added a custom tailwind plugin to set a default background color for the body
    - Extended the theme to include colors for demo purposes
    - Updated App.tsx component to include a styled div utilizing the custom plugin

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,13 @@
 export default function App() {
 
 	return (
-		<h1 className="text-3xl font-bold text-emerald-500 animate-in fade-in duration-1000">
-			Hello TailwindCSS + Vite + React
-		</h1>
+		<>
+			<h1 className="text-3xl font-bold text-emerald-500 animate-in fade-in duration-1000">
+				Hello TailwindCSS + Vite + React
+			</h1>
+			<div className="bg-nasa-black  h-20 mt-4 flex items-center justify-center text-nasa-red text-3xl font-bold">
+				Testando o Plugin
+			</div>
+		</>
 	)
 }
--- vite-project/tailwind.config.ts
+++ vite-project/tailwind.config.ts
@@ -1,5 +1,25 @@
 import type {Config} from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 import twAnimate from  'tailwindcss-animate';
+
+const myPlugin = plugin(({addBase}) => {
+	addBase({
+		body: {
+			background: '#e5e5e0'
+		}
+	})
+}, {
+	theme: {
+		extend: {
+			colors: {
+				nasa: {
+					red: 'red',
+					black: 'black'
+				}
+			}
+		}
+	}
+});
 
 export default {
   content: [
@@ -10,7 +30,8 @@ export default {
     extend: {},
   },
   plugins: [
-		twAnimate
+		twAnimate,
+		myPlugin
 	],
 } satisfies Config;
 
